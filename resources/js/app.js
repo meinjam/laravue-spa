e269bbd53ecfcdc576acfb5acb4b6300ca0fa281
meinjam <injam.bd.jsr@gmail.com>
2020-05-25 11:11:39 +0600

    vue, vuetify and case sensetive setup done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue");
+
+import Vuetify from "../plugins/vuetify";
+
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+
+const app = new Vue({
+    vuetify: Vuetify,
+    el: "#app"
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,13 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+
+var webpackConfig = {
+    plugins: [new VuetifyLoaderPlugin(), new CaseSensitivePathsPlugin()]
+};
+
+mix.webpackConfig(webpackConfig);
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +20,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/sass/app.scss",
+    "public/css"
+);
